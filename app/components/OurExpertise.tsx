3d71a80c6c0d89d286996bfb2821fca85733cae7
Aditya Kumar <aditya.gecv21@gmail.com>
2024-06-29 18:32:15 +0530

    refix image broken

--- app/components/OurExpertise.tsx
+++ app/components/OurExpertise.tsx
@@ -1,26 +1,25 @@
 import React from "react";
 import Card from "./Card";
 import style from "./OurExpertise.module.css";
-import eye_icon from "../../public/asset/eye_img_our_expertise.jpg";
 
 function OurExpertise() {
   return (
     <div>
       <div className={style.our_expertise}>
         <Card
-          imageSrc={eye_icon}
+          imageSrc="/asset/eye_img_our_expertise.jpg"
           imageAlt="Eye Icon"
           title="Automated Eye Surgical Equipment"
           description="Building responsive and modern web applications. Building responsive and modern web applications. Building responsive and modern web applications."
         />
         <Card
-          imageSrc={eye_icon}
+          imageSrc="/asset/eye_img_our_expertise.jpg"
           imageAlt="Tooth icon"
           title="Dental Equipment"
           description="Building responsive and modern web applications. Building responsive and modern web applications. Building responsive and modern web applications."
         />
         <Card
-          imageSrc={eye_icon}
+          imageSrc="/asset/eye_img_our_expertise.jpg"
           imageAlt="other"
           title="Automation"
           description="Building responsive and modern web applications. Building responsive and modern web applications."
--- app/components/OurTeam.tsx
+++ app/components/OurTeam.tsx
@@ -1,27 +1,26 @@
 import React from "react";
 import OurTeamCard from "./OurTeamCard";
 import style from "./OurTeam.module.css";
-import aditya_pic from "../../public/asset/Adityapic.jpeg";
 
 const OurTeam = () => {
   return (
     <div className={style.our_team_card}>
       <OurTeamCard
-        ImageSrc={aditya_pic}
+        ImageSrc="/asset/Adityapic.jpeg"
         ImageAlt="Aditya Pic"
         name="Aditya Kumar"
         position="Web developer Lead"
         description="He is the web Lead. He is so curious about his work and always see forward"
       />
       <OurTeamCard
-        ImageSrc={aditya_pic}
+        ImageSrc="/asset/Adityapic.jpeg"
         ImageAlt="Aditya Pic"
         name="Aditya Kumar"
         position="Web developer Lead"
         description="He is the web Lead. He is so curious about his work and always see forward"
       />
       <OurTeamCard
-        ImageSrc={aditya_pic}
+        ImageSrc="/asset/Adityapic.jpeg"
         ImageAlt="Aditya Pic"
         name="Aditya Kumar"
         position="Web developer Lead"
