de7132c899ed55a1ae329cc757698c2ab8267719
Aditya Kumar <aditya.gecv21@gmail.com>
2024-07-09 15:11:18 +0530

    Footer Responsiveness Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import Hero from "./components/Hero";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import Copyright from "./components/Copyright";
+import Slider from "./components/Slider";
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <Slider />
       <Hero />
       <MainContent />
       <Footer />
