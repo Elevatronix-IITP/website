ba63cecbd3f098abb59ec69accb22f13930724dc
Aditya Kumar <aditya.gecv21@gmail.com>
2024-07-16 12:36:55 +0530

    Map Fixed

--- app/contactUs/page.tsx
+++ app/contactUs/page.tsx
@@ -39,7 +39,7 @@ const contactUs = () => {
               type="text"
               id="subject"
               name="subject"
-              placeholder="Enter Your Subbject "
+              placeholder="Enter Your Subject "
             />
             <label htmlFor="message">Message</label>
             <textarea
@@ -52,6 +52,7 @@ const contactUs = () => {
           </form>
           <div className={style.embed_map}>
             <iframe
+              className={style.iframe}
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3600.0656981202446!2d84.8517746759379!3d25.53618841788586!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed577f694d904f%3A0xec61bf6ba64170a9!2sIncubation%20Centre%2C%20IIT%20patna!5e0!3m2!1sen!2sin!4v1719392988655!5m2!1sen!2sin"
               loading="lazy"
             ></iframe>
